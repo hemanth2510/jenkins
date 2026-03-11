605b4f497d3f82ebe03f6e4a388e727b0d5ca8da
hemanth2510 <hemanthgowda2510@gmail.com>
2026-03-11 11:25:05 +0530

    Add ngrok installation and run instructions
    Added installation and setup instructions for ngrok.

--- script.sh
+++ script.sh
@@ -1,1 +1,17 @@
-echo "hello  from"
+Install ngrok
+
+Update Packages
+
+sudo apt update
+
+Install Snap
+
+sudo apt install snapd -y
+
+Install ngrok
+
+sudo snap install ngrok
+
+Run ngrok
+
+ngrok http 8082
